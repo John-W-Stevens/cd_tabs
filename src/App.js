e72198b7ca697d8a48028355a1a8c3ad3afcae2d
John Stevens <john.william.stevens1@gmail.com>
2020-05-08 14:02:24 -0700

    Completed Tabs assignment. Assumes app.js will pass an array of objects called items. Generates a button for each item and displays content based on which tab the user selects.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
+import "./bootstrap.css"
+
+import Tabs from "./components/Tabs"
+
 
 function App() {
+  const [items, setItems] = useState([
+    {content: "Tab1 content shown here"},
+    {content: "Tab2 content shown here"},
+    {content: "Tab3 content shown here"},
+  ])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Tabs items={items} />
     </div>
   );
 }
